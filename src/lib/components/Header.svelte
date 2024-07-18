8b08016cdb634140fb232068395434c6cc15c19b
Luiz Campos <campos.antoonio@gmail.com>
2024-07-18 17:37:05 -0300

    feat(header): change properties

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { Home, Settings } from 'lucide-svelte';
 </script>
 
@@ -27,11 +27,25 @@
 		--header-width: 4rem;
 	}
 
+	ul {
+		list-style: none;
+	}
+
+	a {
+		text-decoration: inherit;
+	}
+
+	:global(body) {
+		height: 100dvh;
+	}
+
 	header {
 		position: fixed;
 		bottom: 0;
+		left: 0;
+		right: 0;
 		height: var(--header-height);
-		width: 100%;
+		z-index: 100;
 
 		nav {
 			height: 100%;
@@ -45,7 +59,8 @@
 			display: flex;
 			justify-content: space-between;
 			align-items: center;
-			background-color: antiquewhite;
+			background-color: white;
+			border-top: 1px solid hsl(214.3 31.8% 91.4%);
 
 			a {
 				span {
@@ -57,6 +72,7 @@
 
 	@media (width >= 500px) {
 		header {
+			position: sticky;
 			top: 0;
 			left: 0;
 			bottom: 0;
@@ -69,6 +85,8 @@
 				justify-content: start;
 				padding: 1rem 0.5rem;
 				gap: 1rem;
+				border-top: 0;
+				border-right: 1px solid hsl(214.3 31.8% 91.4%);
 			}
 		}
 	}
@@ -85,7 +103,7 @@
 
 			ul {
 				align-items: start;
-				padding: 2rem;
+				padding: 2rem 1rem;
 
 				a {
 					display: flex;
