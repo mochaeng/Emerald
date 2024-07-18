9d2895757dd48eb2b78a5fc3414f6a516eac6f69
Luiz Campos <campos.antoonio@gmail.com>
2024-07-18 19:33:21 -0300

    feat: profile page

---
+++ src/lib/components/PostCard.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import * as Avatar from '$lib/components/ui/avatar';
+
+	type Props = {
+		postImg: {
+			src: string;
+			alt: string;
+		};
+		profileImg: {
+			src: string;
+			alt: string;
+		};
+		profileName: string;
+		profileAt: string;
+		postText: string;
+	};
+
+	let { profileName, profileAt, postText, postImg, profileImg }: Props = $props();
+</script>
+
+<div class="flex h-full w-full flex-col gap-2 border-y-2">
+	<div class="flex items-center justify-between p-2">
+		<div class="flex gap-2">
+			<Avatar.Root>
+				<Avatar.Image src={profileImg.src} alt={profileImg.alt} class="object-cover" />
+				<Avatar.Fallback>CN</Avatar.Fallback>
+			</Avatar.Root>
+			<div>
+				<p>{profileName}</p>
+				<p class="text-sm text-slate-500">@{profileAt}</p>
+			</div>
+		</div>
+		<p>18 Jul</p>
+	</div>
+
+	<div class="flex h-full flex-col gap-2">
+		<p class="p-2 text-start">{postText}</p>
+
+		<div class="lg:h-[800px]">
+			<img
+				src={postImg.src}
+				alt={postImg.alt}
+				class="w-full rounded-sm object-contain lg:h-[800px]"
+			/>
+		</div>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,36 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
+
 	let { children } = $props();
 </script>
 
-<div class="flex h-full flex-col">
+<div class="flex justify-center">
 	<Header />
-	<main class="h-full w-full">
-		{@render children()}
+
+	<main class="flex w-full max-w-[1016px] pb-16 md:pb-0">
+		<div class="w-full max-w-[632px] border-x-[1px]">
+			{@render children()}
+		</div>
+		<div class="left-footer hidden w-[384px] max-w-[384px]">
+			<a href="/">About</a>
+			<a href="/">About</a>
+			<a href="/">About</a>
+		</div>
 	</main>
 </div>
 
 <style>
 	:global(body) {
 		height: 100dvh;
+		overflow: auto;
+	}
+
+	@media (width >= 1004px) {
+		.left-footer {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
 	}
 </style>
